54e89c12f97557a92ebe4ccbb191c73028db1193
Vitor Mendes Oliverio <vitormendes4072@gmail.com>
2022-08-10 21:23:01 -0300

    Create quatroOperacoes.c

---
+++ quatroOperacoes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//ex1
+void main() {
+    int a, b;
+    printf("Insira o primeiro número: \n");
+    scanf("%i", &a);
+    scanf("%i", &b);
+    int adicao = a + b;
+    int subtracao = a - b;
+    int divisao = a / b;
+    int multiplicacao = a * b;
+    printf("Adição = %i \n", adicao);
+    printf("Subtração = %i \n", subtracao);
+    printf("Dividsão = %i \n", divisao);
+    printf("multiplicação = %i \n", multiplicacao);
+}
