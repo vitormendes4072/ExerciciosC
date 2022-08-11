306bf409da1f9d8ea3ccda5a671f5b5a46735a1f
Vitor Mendes Oliverio <vitormendes4072@gmail.com>
2022-08-10 21:31:51 -0300

    Create aumentoSalario.c

---
+++ aumentoSalario.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//ex3
+void main() {
+    float salario = 4500;
+    float porcentagemAumento = 0.12;
+    float aumento = salario * porcentagemAumento;
+    printf("Aumento = %f \n", aumento);
+}
